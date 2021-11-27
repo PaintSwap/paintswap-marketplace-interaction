d2c7115c549152ad53fed9d150ad3c92cbc558a3
pdroid <84405831+pdroid@users.noreply.github.com>
2021-11-27 07:11:30 +0000

    Marketplace V2 listen support (#1)
    * Streamlined Sold event
    
    * Better calculation of price in a bundle
    
    * Make the conversion to bundle optional
    
    * Refactor with interface inheritance, avoiding some code repetition
    
    * Rename index to listen
    
    * Extract static util methods to another class
    
    * Add unsold listener
    
    * Add duration extension (UpdateEndTime)
    
    * Support for cancelled
    
    * Fixed PriceUpdate handling, no longer as individual NFTs either, only full bundle
    
    * NewBid and NewOffer
    
    Co-authored-by: pdroid <>

---
+++ lib/marketplaceV2Types.ts
@@ -0,0 +1,89 @@
+import { BigNumber } from "ethers"
+
+export interface BundleBase {
+    marketplaceId: BigNumber
+    nfts: Array<string> // the collections
+    tokenIds: Array<BigNumber>
+    amountBatches: Array<BigNumber> // individual amounts within each bundle
+}
+
+export interface BundlePriced extends BundleBase {
+    amount: BigNumber
+    price: BigNumber
+}
+
+export interface Base {
+    marketplaceId: BigNumber
+    collection: string
+    tokenID: BigNumber
+    amountInBundle: BigNumber
+}
+
+export interface Priced extends Base {
+    amount: BigNumber
+    price: BigNumber
+}
+
+// -----------
+
+// A type describing a new listing
+interface NewSaleBase {
+    duration: BigNumber
+    isAuction: boolean
+    isNSFW: boolean
+}
+
+export interface NewBundleSale extends BundlePriced, NewSaleBase { }
+
+// NewBundleSale splits into NewSale
+export interface NewSale extends Priced, NewSaleBase { }
+
+// -----------
+
+// A type describing a successful sale (e.g. direct buy, offer accepted or auction finished with a bid)
+interface SoldBase {
+    buyer: string
+    seller: string
+}
+
+export interface BundleSold extends BundlePriced, SoldBase { }
+
+// BundleSold splits into Sold
+export interface Sold extends Priced, SoldBase { }
+
+// -----------
+
+export interface BundleUnsold extends BundleBase { }
+
+// BundleUnsold splits into Unsold
+export interface Unsold extends Base { }
+
+// -----------
+
+export interface BundlePriceUpdate {
+    marketplaceId: BigNumber
+    price: BigNumber
+}
+
+// -----------
+
+export interface DurationExtended {
+    marketplaceId: BigNumber
+    endTime: BigNumber
+}
+
+// -----------
+
+export interface NewBid {
+    marketplaceId: BigNumber
+    bidder: string
+    bid: BigNumber
+    nextMinimum: BigNumber
+}
+
+export interface NewOffer {
+    marketplaceId: BigNumber
+    offerrer: string
+    offer: BigNumber
+    nextMinimum: BigNumber
+}
