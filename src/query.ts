2106bbfb5e0ec356293f2ae892dbb44f1185f256
0xCheeseKnight <0xcheeseknight@protonmail.com>
2021-11-27 14:54:52 -0400

    Run prettier to fix indentation issues

--- src/query.ts
+++ src/query.ts
@@ -1,20 +1,18 @@
 #!/usr/bin/env ts-node
 
-import { ethers } from "ethers"
-import MarketplaceV2 from "./lib/marketplaceV2"
+import { ethers } from 'ethers'
+import MarketplaceV2 from './lib/marketplaceV2'
 
-const provider = new ethers.providers.JsonRpcProvider(
-    "https://rpc.ftm.tools/"
-);
+const provider = new ethers.providers.JsonRpcProvider('https://rpc.ftm.tools/')
 
 const marketplace = new MarketplaceV2(provider)
 
 const QueryID = 68897 // https://paintswap.finance/marketplace/68897
 
 marketplace.getSaleDetails(ethers.BigNumber.from(QueryID)).then((details) => {
-    console.log(`Sale details for sale ${QueryID}:\n`, details)
+  console.log(`Sale details for sale ${QueryID}:\n`, details)
 })
 
 marketplace.getNextMinimumBidOrOffer(ethers.BigNumber.from(QueryID)).then((next) => {
-    console.log(`Next minimum bid or offer for sale ${QueryID}: `, `${ethers.utils.formatEther(next.toString())} $FTM`)
+  console.log(`Next minimum bid or offer for sale ${QueryID}: `, `${ethers.utils.formatEther(next.toString())} $FTM`)
 })
